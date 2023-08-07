48c3d93f198a825857a3642b26204acd9828f8c2
Felix <jonfelixrico@gmail.com>
2023-08-08 00:59:05 +0800

    Implement error handling

--- src/app/mods/[filename]/route.ts
+++ src/app/mods/[filename]/route.ts
@@ -1,11 +1,24 @@
 import { ContextParams } from '@/types/ContextParams.type'
-import { getFile } from '@/utils/sftp-utils'
+import { FileNotFoundError, getFile } from '@/utils/sftp-utils'
 import { NextResponse } from 'next/server'
 
 export async function GET(
   _: Request,
-  context: ContextParams<{ filename: string }>
+  { params: { filename } }: ContextParams<{ filename: string }>
 ) {
-  const fileBuffer = await getFile(context.params.filename)
-  return new NextResponse(fileBuffer)
+  try {
+    const fileBuffer = await getFile(filename)
+    return new NextResponse(fileBuffer)
+  } catch (e) {
+    if (e instanceof FileNotFoundError) {
+      return NextResponse.json(
+        {
+          error: `${filename} was not found in the modlist`,
+        },
+        {
+          status: 404,
+        }
+      )
+    }
+  }
 }
