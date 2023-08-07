a0f28b054a8376fdf00a8d8b15c65df1a03e60b8
Felix <jonfelixrico@gmail.com>
2023-08-07 23:53:10 +0800

    feat: implement file list (#3)
    * Install ssh2
    
    * Create .env.development file
    
    * Use a more specific library
    
    * Create base utils
    
    * Fix types
    
    * Create listFiles method
    
    * Make async
    
    * Return the proper values
    
    * Replace with actual content
    
    * Create next config
    
    * Implement
    
    * Use the app way of server side generation
    
    * Expose the type
    
    * Use tables
    
    * Remove initial styles
    
    * Install lodash
    
    * Use lodash
    
    * Use correct table tags

---
+++ src/utils/sftp-utils.ts
@@ -0,0 +1,46 @@
+import Client from 'ssh2-sftp-client'
+
+async function getClient() {
+  const host = process.env.SFTP_HOST
+  const port = process.env.SFTP_PORT
+  const username = process.env.SFTP_USERNAME
+  const password = process.env.SFTP_PASSWORD
+
+  if (
+    typeof host === 'undefined' ||
+    typeof port === 'undefined' ||
+    typeof password === 'undefined' ||
+    typeof username === 'undefined'
+  ) {
+    throw new Error('bad config')
+  }
+
+  const client = new Client()
+  await client.connect({
+    host,
+    port: parseInt(port),
+    username,
+    password,
+  })
+  return client
+}
+
+async function executeInClient<T>(
+  toExec: (client: Client) => Promise<T> | T
+): Promise<T> {
+  let client: Client | null = null
+
+  try {
+    client = await getClient()
+    return await toExec(client)
+  } finally {
+    if (client) {
+      client.end()
+    }
+  }
+}
+
+export type FileInfo = Client.FileInfo
+export async function listFiles(): Promise<FileInfo[]> {
+  return await executeInClient(async (client) => await client.list('/mods'))
+}
