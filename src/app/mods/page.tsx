7829079575e8e6866aac683f640960a8f54c6f89
Felix <jonfelixrico@gmail.com>
2023-08-08 02:20:16 +0800

    Integrate ant's table

--- src/app/mods/page.tsx
+++ src/app/mods/page.tsx
@@ -1,38 +1,13 @@
-import { FileInfo, listFiles } from '@/utils/sftp-utils'
-import { round } from 'lodash'
+import { listFiles } from '@/utils/sftp-utils'
+import FileTable from './FileTable'
 
-function Row({ name, size }: Pick<FileInfo, 'name' | 'size'>) {
-  return (
-    <tr>
-      <td>
-        <a href={`/mods/${name}`} download>
-          {name}
-        </a>
-      </td>
-      <td>{round(size / 1000 / 1000, 2)} MB</td>
-    </tr>
-  )
-}
 
 export default async function Mods() {
   const files = await listFiles()
 
   return (
     <div className="h-full w-full overflow-auto p-6">
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Size</th>
-          </tr>
-        </thead>
-
-        <tbody>
-          {files.map(({ name, size }, index) => (
-            <Row name={name} size={size} key={index} />
-          ))}
-        </tbody>
-      </table>
+      <FileTable files={files} />
     </div>
   )
 }
