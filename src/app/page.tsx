59dda91e8f510251003bbdcbd8929a5ab692814e
Felix <jonfelixrico@gmail.com>
2023-08-07 23:06:26 +0800

    Implement

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,27 @@
 import { listFiles } from '@/utils/sftp-utils'
+import { GetServerSideProps } from 'next'
 
-export default async function Home() {
-  const files = await listFiles()
+export default function Home({
+  files,
+}: {
+  files: Awaited<ReturnType<typeof listFiles>>
+}) {
   return (
     <main>
-      {files.map((file, index) => (
+      {files?.map((file, index) => (
         <div key={index}>{JSON.stringify(file)}</div>
       ))}
     </main>
   )
+}
+
+export const getServerSideProps: GetServerSideProps<{
+  files: Awaited<ReturnType<typeof listFiles>>
+}> = async () => {
+  const files = await listFiles()
+  return {
+    props: {
+      files,
+    },
+  }
 }
