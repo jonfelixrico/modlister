bfe167a6c9f30c668bcdf603a0e508485101c63b
Felix <jonfelixrico@gmail.com>
2023-08-08 00:45:23 +0800

    Remove mime type

--- src/app/mods/[filename]/route.ts
+++ src/app/mods/[filename]/route.ts
@@ -1,9 +1,11 @@
 import { ContextParams } from '@/types/ContextParams.type'
 import { getFile } from '@/utils/sftp-utils'
+import { NextResponse } from 'next/server'
 
 export async function GET(
   _: Request,
   context: ContextParams<{ filename: string }>
 ) {
-  return await getFile(context.params.filename)
+  const fileBuffer = await getFile(context.params.filename)
+  return new NextResponse(fileBuffer)
 }
