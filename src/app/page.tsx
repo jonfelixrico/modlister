302a95a16ea04077bc73125e2bb30fcf912d407e
Felix <jonfelixrico@gmail.com>
2023-08-08 01:54:38 +0800

    Create the content of the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
+import { Divider } from 'antd'
+
 export default function Home() {
-  return <div>Test</div>
+  return (
+    <div className="h-full w-full flex flex-col items-center justify-center">
+      <div className="flex flex-col items-center">
+        <h2 className="text-5xl">Download the Bundle</h2>
+        <Divider>or</Divider>
+        <h2 className="text-3xl">Download Individually</h2>
+      </div>
+    </div>
+  )
 }
