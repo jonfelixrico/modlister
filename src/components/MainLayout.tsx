35b1e3c441503e6ae2e2b9c6ca1543b10ef70206
Felix <jonfelixrico@gmail.com>
2023-08-08 01:46:48 +0800

    Create initial layout

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -6,8 +6,8 @@ import { Layout, Typography } from 'antd'
 export default function MainLayout(props: { children: ReactNode }) {
   return (
     <Layout className="w-screen h-screen">
-      <Layout.Header>
-        <Typography.Title className="mb-0">CSA ForgeMC</Typography.Title>
+      <Layout.Header className="flex flex-row items-center">
+        <h1 className="text-3xl text-white">CSA ForgeMC</h1>
       </Layout.Header>
       <Layout.Content>{props.children}</Layout.Content>
     </Layout>
