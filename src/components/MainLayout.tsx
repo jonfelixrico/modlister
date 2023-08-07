0d03ec9276d0bdefe4d453c2d024bb58819970ef
Felix <jonfelixrico@gmail.com>
2023-08-08 01:39:45 +0800

    Add title

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -1,12 +1,14 @@
 'use client'
 
 import { ReactNode } from 'react'
-import { Layout } from 'antd'
+import { Layout, Typography } from 'antd'
 
 export default function MainLayout(props: { children: ReactNode }) {
   return (
     <Layout className="w-screen h-screen">
-      <Layout.Header>CSA ForgeMC</Layout.Header>
+      <Layout.Header>
+        <Typography.Title className="mb-0">CSA ForgeMC</Typography.Title>
+      </Layout.Header>
       <Layout.Content>{props.children}</Layout.Content>
     </Layout>
   )
