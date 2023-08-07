123f96777b4eb77c69715efde4690092bafe5acd
Felix <jonfelixrico@gmail.com>
2023-08-07 23:58:46 +0800

    Adjust the structure

---
+++ src/app/mods/page.tsx
@@ -0,0 +1,34 @@
+import { FileInfo, listFiles } from '@/utils/sftp-utils'
+import { round } from 'lodash'
+
+function Row({ name, size }: Pick<FileInfo, 'name' | 'size'>) {
+  return (
+    <tr>
+      <td>{name}</td>
+      <td>{round(size / 1000 / 1000, 2)} MB</td>
+    </tr>
+  )
+}
+
+export default async function Mods() {
+  const files = await listFiles()
+
+  return (
+    <main>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Size</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {files.map(({ name, size }, index) => (
+            <Row name={name} size={size} key={index} />
+          ))}
+        </tbody>
+      </table>
+    </main>
+  )
+}
