c0cf674ecb7114308c024e851eaec258d31cd1c0
Felix <jonfelixrico@gmail.com>
2023-08-08 00:20:17 +0800

    Make route.ts return a file

--- src/app/mods/[filename]/route.ts
+++ src/app/mods/[filename]/route.ts
@@ -1,8 +1,9 @@
 import { ContextParams } from '@/types/ContextParams.type'
+import { getFile } from '@/utils/sftp-utils'
 
 export async function GET(
-  request: Request,
+  _: Request,
   context: ContextParams<{ filename: string }>
 ) {
-  return null
+  return await getFile(context.params.filename)
 }
