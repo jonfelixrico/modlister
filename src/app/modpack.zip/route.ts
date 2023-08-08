ca7f1511902984e5e864a62ded69634e1e0e9920
Felix <jonfelixrico@gmail.com>
2023-08-08 22:13:40 +0800

    Adjust

--- src/app/modpack.zip/route.ts
+++ src/app/modpack.zip/route.ts
@@ -1,46 +1,13 @@
-import { getFile, listFiles } from '@/utils/sftp-utils'
-import { generateZip } from '@/utils/zip-utils'
+import { generateModpack } from '@/utils/mod-utils'
 import ExpiryMap from 'expiry-map'
 import { NextResponse } from 'next/server'
-import pLimit from 'p-limit'
+
 import pMemoize from 'p-memoize'
 
-async function generateBundle() {
-  const fileInfos = await listFiles()
-  console.debug('fetched info for %s files', fileInfos.length)
-
-  const limit = pLimit(3)
-  const dlPromises = fileInfos.map((file) => {
-    return limit(() => getFile(file.name))
-  })
-  const results = await Promise.all(dlPromises)
-  console.debug('download complete')
-
-  const zip = await generateZip(
-    results.map((buffer, index) => {
-      return {
-        buffer,
-        filename: fileInfos[index].name,
-      }
-    })
-  )
-  console.debug('zip complete')
-
-  return zip
-}
-
-const memGenerateBundle = pMemoize((_: string) => generateBundle(), {
-  cache: new ExpiryMap(60_000 * 10),
+const memGenerateBundle = pMemoize(generateModpack, {
+  cache: new ExpiryMap(60_000),
 })
 
-export async function GET(req: Request) {
-  const url = new URL(req.url)
-
-  console.log('started')
-  const fileBuffer = await memGenerateBundle(
-    url.searchParams.get('lastModified') ?? 'unknown'
-  )
-  console.log('ended')
-
-  return new NextResponse(fileBuffer)
+export async function GET() {
+  return new NextResponse(await memGenerateBundle())
 }
