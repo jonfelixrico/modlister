ee057eff1b8ff9307b65d874ae38752f323074b3
Felix <jonfelixrico@gmail.com>
2023-08-08 19:26:06 +0800

    Rename to modpack

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
           type="primary"
           size="large"
           style={{ height: 'auto' }}
-          href="bundles"
+          href="modpack.zip"
           download
         >
           <h2 className="text-4xl">Download the Bundle</h2>
