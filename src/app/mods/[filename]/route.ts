71c79e192833bb3b1f2a15826887c8ab6f2292d3
Felix <jonfelixrico@gmail.com>
2023-08-08 18:24:47 +0800

    feat: optimize by memoizing SFTP methods (#6)
    * Install p-memoize
    
    * Install expiry-map
    
    * Use p-memoize with getFIle
    
    * Memoize listFiles
    
    * Adjust how the memoization was written

--- src/app/mods/[filename]/route.ts
+++ src/app/mods/[filename]/route.ts
@@ -1,13 +1,17 @@
 import { ContextParams } from '@/types/ContextParams.type'
 import { FileNotFoundError, getFile } from '@/utils/sftp-utils'
+import ExpiryMap from 'expiry-map'
 import { NextResponse } from 'next/server'
+import pMemoize from 'p-memoize'
+
+const memGetFile = pMemoize(getFile, { cache: new ExpiryMap(60_000 * 5) })
 
 export async function GET(
   _: Request,
   { params: { filename } }: ContextParams<{ filename: string }>
 ) {
   try {
-    const fileBuffer = await getFile(filename)
+    const fileBuffer = await memGetFile(filename)
     return new NextResponse(fileBuffer)
   } catch (e) {
     if (e instanceof FileNotFoundError) {
