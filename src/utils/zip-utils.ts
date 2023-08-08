889a336cc66e90f395c057cf485184797479f874
Felix <jonfelixrico@gmail.com>
2023-08-08 18:50:11 +0800

    Create zip utils

---
+++ src/utils/zip-utils.ts
@@ -0,0 +1,39 @@
+import archiver from 'archiver'
+import MemoryStream from 'memorystream'
+import { getStreamAsBuffer } from 'get-stream'
+
+export interface ArchiveFile {
+  filename: string
+  buffer: Buffer
+}
+
+export function generateZip(files: ArchiveFile[]): Promise<Buffer> {
+  return new Promise((resolve, reject) => {
+    const output = new MemoryStream()
+    const archive = archiver('zip')
+
+    output.on('finish', async () => {
+      try {
+        resolve(await getStreamAsBuffer(output))
+      } catch (e) {
+        reject(e)
+      }
+    })
+    archive.on('error', (err) => {
+      reject(err)
+      output.end()
+    })
+    archive.on('warning', (err) => {
+      if (err.code !== 'ENOENT') {
+        reject(err)
+        output.end()
+      }
+    })
+
+    archive.pipe(output)
+    for (const { buffer, filename } of files) {
+      archive.append(buffer, { name: filename })
+    }
+    archive.finalize()
+  })
+}
