dd77c5f0523231a9e43cf743acedfcece69be583
Felix <jonfelixrico@gmail.com>
2023-08-08 18:11:36 +0800

    Memoize listFiles

--- src/app/mods/page.tsx
+++ src/app/mods/page.tsx
@@ -1,9 +1,12 @@
 import { listFiles } from '@/utils/sftp-utils'
 import FileTable from './FileTable'
+import pMemoize from 'p-memoize'
+import ExpiryMap from 'expiry-map'
 
+const memListFiles = pMemoize(listFiles, { cache: new ExpiryMap(60_000 * 1) })
 
 export default async function Mods() {
-  const files = await listFiles()
+  const files = await memListFiles()
 
   return (
     <div className="h-full w-full overflow-auto p-6">
