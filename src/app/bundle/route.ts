3359fc9a8953f94cb2a8286421e065a4231d8ca0
Felix <jonfelixrico@gmail.com>
2023-08-09 09:49:05 +0800

    Create response

--- src/app/bundle/route.ts
+++ src/app/bundle/route.ts
@@ -9,13 +9,27 @@ import { NextResponse } from 'next/server'
 import pMemoize from 'p-memoize'
 
 const memGetBundle = pMemoize(getBundle, { cache: new ExpiryMap(1000 * 10) })
-
 export async function GET(
   _: Request,
   context: ContextParams<{ timestamp: string }>
 ) {
   const { timestamp } = context.params
-  return new NextResponse(await memGetBundle(timestamp))
+  if (!(await checkIfBundleExists(timestamp))) {
+    return NextResponse.json(
+      {
+        name: 'Bundle not found',
+      },
+      {
+        status: 404,
+      }
+    )
+  }
+
+  return new NextResponse(await memGetBundle(timestamp), {
+    headers: {
+      'Content-Disposition': `attachment; filename="${timestamp}.zip"`,
+    },
+  })
 }
 
 const memCreateBundle = pMemoize(createBundle, {
