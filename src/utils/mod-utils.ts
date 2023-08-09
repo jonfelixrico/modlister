cc2db5fbb39b7ecc0481d0e5d5c4a780057eb8f8
Felix <jonfelixrico@gmail.com>
2023-08-09 11:09:55 +0800

    Remove unused

--- src/utils/mod-utils.ts
+++ src/utils/mod-utils.ts
@@ -1,7 +1,6 @@
 import pMemoize from 'p-memoize'
 import ExpiryMap from 'expiry-map'
 import { getFile, listFiles } from './sftp-utils'
-import { archiveFilesAsBuffer } from './zip-utils'
 
 export const memListFiles = pMemoize(listFiles, {
   cache: new ExpiryMap(1000 * 60),
@@ -10,27 +9,3 @@ export const memListFiles = pMemoize(listFiles, {
 export const memGetFile = pMemoize(getFile, {
   cache: new ExpiryMap(1000 * 60 * 60),
 })
-
-export async function generateModpack() {
-  const fileInfos = await memListFiles()
-  console.debug('fetched info for %s files', fileInfos.length)
-
-  const downloaded: Buffer[] = []
-  for (const { name } of fileInfos) {
-    downloaded.push(await memGetFile(name))
-    console.debug('acquired %s', name)
-  }
-  console.debug('download complete')
-
-  const zip = await archiveFilesAsBuffer(
-    downloaded.map((buffer, index) => {
-      return {
-        buffer,
-        filename: fileInfos[index].name,
-      }
-    })
-  )
-  console.debug('zip complete')
-
-  return zip
-}
