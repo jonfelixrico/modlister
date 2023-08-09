f4c8a1417880b263effc4fc53aa7f98956927d74
Felix <jonfelixrico@gmail.com>
2023-08-09 10:23:36 +0800

    Adjust route

--- src/app/bundle/[timestamp]/route.ts
+++ src/app/bundle/[timestamp]/route.ts
@@ -1,8 +1,4 @@
-import {
-  checkIfBundleExists,
-  createBundle,
-  getBundle,
-} from '@/services/filestore-service'
+import { checkIfBundleExists, getBundle } from '@/services/filestore-service'
 import { ContextParams } from '@/types/ContextParams.type'
 import ExpiryMap from 'expiry-map'
 import { NextResponse } from 'next/server'
@@ -29,28 +25,5 @@ export async function GET(
     headers: {
       'Content-Disposition': `attachment; filename="${timestamp}.zip"`,
     },
-  })
-}
-
-const memCreateBundle = pMemoize(createBundle, {
-  cache: new ExpiryMap(1000 * 60 * 30),
-})
-
-export async function POST(
-  _: Request,
-  context: ContextParams<{ timestamp: string }>
-) {
-  const { timestamp } = context.params
-
-  if (await checkIfBundleExists(timestamp)) {
-    return NextResponse.json({
-      status: 'DONE',
-    })
-  }
-
-  memCreateBundle(timestamp)
-
-  return NextResponse.json({
-    status: 'QUEUED',
   })
 }
