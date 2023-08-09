310c0db341ecae406cbb0bbfe9e18dcea7267abd
Felix <jonfelixrico@gmail.com>
2023-08-09 10:25:04 +0800

    Make page do post

--- src/app/modpack/page.tsx
+++ src/app/modpack/page.tsx
@@ -1,7 +1,13 @@
-import { checkIfBundleExists } from '@/services/filestore-service'
+import { checkIfBundleExists, createBundle } from '@/services/filestore-service'
 import { memListFiles } from '@/utils/mod-utils'
 import { Button } from 'antd'
 import ModpackSplash from './ModpackSplash'
+import pMemoize from 'p-memoize'
+import ExpiryMap from 'expiry-map'
+
+const memCreateBundle = pMemoize(createBundle, {
+  cache: new ExpiryMap(1000 * 60 * 30),
+})
 
 async function getLastModDt() {
   const files = await memListFiles()
@@ -15,6 +21,11 @@ function DownloadBtn(props: { timestamp: number }) {
 export default async function ModpackPage() {
   const lastModDt = await getLastModDt()
   const bundleExists = await checkIfBundleExists(lastModDt)
+
+  if (!bundleExists) {
+    // intended to be done asynchronously
+    memCreateBundle(String(lastModDt))
+  }
 
   return (
     <div className="h-full w-full flex flex-col justify-center items-center">
