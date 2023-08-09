cd5e0b98bc1ec8a2a85534a2b0facedf245ca365
Felix <jonfelixrico@gmail.com>
2023-08-09 10:03:21 +0800

    Create the download page

--- src/app/modpack/page.tsx
+++ src/app/modpack/page.tsx
@@ -1,3 +1,23 @@
+import { checkIfBundleExists } from '@/services/filestore-service'
+import { memListFiles } from '@/utils/mod-utils'
+import { Button } from 'antd'
+
+async function getLastModDt() {
+  const files = await memListFiles()
+  return Math.max(...files.map(({ modifyTime }) => modifyTime))
+}
+
+function DownloadBtn(props: { timestamp: number }) {
+  return <Button href={`bundle/${props.timestamp}`}>Download Bundle</Button>
+}
+
 export default async function ModpackPage() {
-  return <div>teka muna</div>
+  const lastModDt = await getLastModDt()
+  const bundleExists = await checkIfBundleExists(lastModDt)
+
+  return (
+    <div className="h-full w-full flex flex-col justify-center items-center">
+      {bundleExists ? <DownloadBtn timestamp={lastModDt} /> : null}
+    </div>
+  )
 }
