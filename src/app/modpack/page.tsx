6c1a171cc765a9a75d84fb0683c33b12a058599c
Felix <jonfelixrico@gmail.com>
2023-08-09 09:52:09 +0800

    Adjust multiple things

---
+++ src/app/modpack/page.tsx
@@ -0,0 +1,3 @@
+export default async function ModpackPage() {
+  return <div>teka muna</div>
+}
--- src/utils/mod-utils.ts
+++ src/utils/mod-utils.ts
@@ -4,11 +4,11 @@ import { getFile, listFiles } from './sftp-utils'
 import { archiveFilesAsBuffer } from './zip-utils'
 
 export const memListFiles = pMemoize(listFiles, {
-  cache: new ExpiryMap(60_000 * 1),
+  cache: new ExpiryMap(1000 * 60),
 })
 
 export const memGetFile = pMemoize(getFile, {
-  cache: new ExpiryMap(60_000 * 60),
+  cache: new ExpiryMap(1000 * 60 * 60),
 })
 
 export async function generateModpack() {
