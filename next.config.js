efd46fa9e3b57395c640e4d89695b4dd39af312e
Felix <jonfelixrico@gmail.com>
2023-08-09 22:08:58 +0800

    Configure

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
+const { configureRuntimeEnv } = require('next-runtime-env/build/configure')
+configureRuntimeEnv()
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
