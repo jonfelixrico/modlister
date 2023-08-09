5cfaeea0286590998360200dccbd2fad9034242f
Felix <jonfelixrico@gmail.com>
2023-08-09 10:32:37 +0800

    Implement polling

--- src/app/modpack/ModpackSplash.tsx
+++ src/app/modpack/ModpackSplash.tsx
@@ -7,22 +7,26 @@ import { useInterval } from 'react-use'
 export default function ModpackSplash(props: { timestamp: number }) {
   const [isReady, setIsReady] = useState(false)
   useInterval(
-    () => {
+    async () => {
       if (isReady) {
         return
       }
 
-      // TODO do something
+      const response = await fetch(`bundle/${props.timestamp}`, {
+        cache: 'no-cache',
+      })
+
+      if (response.ok) {
+        setIsReady(true)
+      } else {
+        console.log('Not yet ready. HTTP status %d', response.status)
+      }
     },
-    isReady ? null : 5000
+    isReady ? null : 5_000
   )
 
   if (isReady) {
-    return (
-      <Button href={`bundle/${props.timestamp}`}>
-        Download Bundle
-      </Button>
-    )
+    return <Button href={`bundle/${props.timestamp}`}>Download Bundle</Button>
   } else {
     return (
       <div className="flex flex-row gap-5 items-center">
