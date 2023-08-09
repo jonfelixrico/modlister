21f4ddc3bd306dc02e86f05622cd54bc5257fd5e
Felix <jonfelixrico@gmail.com>
2023-08-09 22:29:00 +0800

    Revert "Configure"
    This reverts commit efd46fa9e3b57395c640e4d89695b4dd39af312e.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,3 @@
-const { configureRuntimeEnv } = require('next-runtime-env/build/configure')
-configureRuntimeEnv()
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
