b097e792154525522327a564d1430a441b599189
Felix <jonfelixrico@gmail.com>
2023-08-09 09:33:45 +0800

    Adjust

--- src/app/bundle/route.ts
+++ src/app/bundle/route.ts
@@ -1,6 +1,24 @@
-import { createZippedFilestore } from '@/services/filestore-service'
+import { createZippedFilestore, getBundle } from '@/services/filestore-service'
+import { ContextParams } from '@/types/ContextParams.type'
+import ExpiryMap from 'expiry-map'
 import { NextResponse } from 'next/server'
+import pMemoize from 'p-memoize'
 
-export async function GET(_: Request) {
-  return new NextResponse(await createZippedFilestore())
+const memGetBundle = pMemoize(getBundle, { cache: new ExpiryMap(1000 * 10) })
+
+export async function GET(
+  _: Request,
+  context: ContextParams<{ timestamp: string }>
+) {
+  const { timestamp } = context.params
+  return new NextResponse(await memGetBundle(timestamp))
 }
+
+export async function POST(
+  _: Request,
+  {
+    params: { timestamp },
+  }: ContextParams<{
+    timestamp: string
+  }>
+) {}
--- src/app/bundle/status/route.ts
+++
@@ -1,8 +0,0 @@
-import { isSynced } from '@/services/filestore-service'
-import { NextResponse } from 'next/server'
-
-export async function GET() {
-  return NextResponse.json({
-    synced: await isSynced(),
-  })
-}
