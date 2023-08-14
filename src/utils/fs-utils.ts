5e211cacec73aaf99814a353d767953aba9e4469
Felix <jonfelixrico@gmail.com>
2023-08-14 23:43:49 +0800

    Create `rm`

--- src/utils/fs-utils.ts
+++ src/utils/fs-utils.ts
@@ -3,3 +3,4 @@ import fs from 'graceful-fs'
 
 export const readFile = Bluebird.promisify(fs.readFile)
 export const writeFile = Bluebird.promisify(fs.writeFile)
+export const rm = Bluebird.promisify(fs.rm)
