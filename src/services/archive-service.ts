f2f1cb3330be9cc600bea37f8d110664ee29676b
Felix <jonfelixrico@gmail.com>
2023-08-16 22:53:39 +0800

    Adjust dirs

--- src/services/archive-service.ts
+++ src/services/archive-service.ts
@@ -4,8 +4,8 @@ import { archiveFilesAsBuffer } from '@/utils/zip-utils'
 import { glob } from 'glob'
 import path from 'path'
 
-const DIR = 'files/archives'
-const ARCHIVE_PATH = path.join(process.cwd(), DIR)
+const RELATIVE_DIR = 'files/archives'
+const ABSOLUTE_DIR = path.join(process.cwd(), RELATIVE_DIR)
 
 export async function createArchive(archiveFilename: string, files: File[]) {
   const buffer = await archiveFilesAsBuffer(
@@ -19,17 +19,17 @@ export async function createArchive(archiveFilename: string, files: File[]) {
 
   const filename = `${archiveFilename}.zip`
   console.debug('archiving mods into %s', filename)
-  await writeFile(path.join(ARCHIVE_PATH, filename), buffer)
+  await writeFile(path.join(ABSOLUTE_DIR, filename), buffer)
   console.log('created archive %s', filename)
 }
 
 export async function getArchive(filename: string) {
-  return await readFile(path.join(ARCHIVE_PATH, filename))
+  return await readFile(path.join(ABSOLUTE_DIR, filename))
 }
 
 export async function checkIfArchiveExists(
   timestamp: number | string
 ): Promise<boolean> {
-  const results = await glob(path.join(DIR, `${timestamp}.zip`))
+  const results = await glob(path.join(RELATIVE_DIR, `${timestamp}.zip`))
   return results.length > 0
 }
