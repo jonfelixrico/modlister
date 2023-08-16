e49dcb5e23754ddf81c3ea26c74532d995263432
Felix <jonfelixrico@gmail.com>
2023-08-16 23:24:41 +0800

    Create aux mod service

---
+++ src/services/aux-mod-service.ts
@@ -0,0 +1,32 @@
+import { File } from '@/types/File.interface'
+import { readFile } from '@/utils/fs-utils'
+import { glob } from 'glob'
+import pLimit from 'p-limit'
+import path from 'path'
+
+const RELATIVE_DIR = 'files/aux-mods'
+const ABSOLUTE_DIR = path.join(process.cwd(), RELATIVE_DIR)
+
+export async function getAuxModFilenames() {
+  return (await glob(`${RELATIVE_DIR}/*.jar`)).map((globPath) =>
+    path.basename(globPath)
+  )
+}
+
+export async function getAuxModContents(): Promise<File[]> {
+  const filenames = await getAuxModFilenames()
+
+  const limited = pLimit(5)
+  const buffers = await Promise.all(
+    filenames.map((filename) =>
+      limited(() => readFile(path.join(ABSOLUTE_DIR, filename)))
+    )
+  )
+
+  return buffers.map((buffer, index) => {
+    return {
+      data: buffer,
+      filename: filenames[index],
+    }
+  })
+}
